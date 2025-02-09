e33cb4e01f28945c0e3b1343597bc6270861f652
farhan1515 <farhananwar784@gmail.com>
2025-02-10 01:44:13 +0530

    careers

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
-import Careers from "./components/careers";
+import Careers from "./components/Careers";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
 
